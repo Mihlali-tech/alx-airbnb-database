8d3bdc8299a756ca2ecfc9d94297b4d7a015be2b
Mihlali Ncayo <mihlalincayo84@gmail.com>
2025-07-01 23:05:06 +0200

    Fix: Add missing columns in LEFT JOIN query for reviews

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -19,10 +19,13 @@ SELECT
     p.location,
     p.pricepernight,
     r.review_id,
+    r.user_id,
     r.rating,
-    r.comment
+    r.comment,
+    r.created_at
 FROM Property p
 LEFT JOIN Review r ON p.property_id = r.property_id;
+
 
 --a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT 
