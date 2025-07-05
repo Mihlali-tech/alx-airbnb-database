6ef42a0c5930add334a620311305a45556c503b0
Mihlali Ncayo <mihlalincayo84@gmail.com>
2025-07-05 13:34:50 +0200

    Refactor performance queries: include EXPLAIN for analysis and additional filter condition with AND

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,5 @@
--- Initial query (before optimization)
+-- EXPLAIN initial query (before optimization)
+EXPLAIN
 SELECT
     b.booking_id,
     b.start_date,
@@ -17,11 +18,12 @@ SELECT
     pay.payment_date,
     pay.payment_method
 FROM Booking b
-INNER JOIN User u ON b.user_id = u.user_id
+INNER JOIN [User] u ON b.user_id = u.user_id
 INNER JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
 
--- Optimized query (final version)
+-- EXPLAIN optimized query (final version)
+EXPLAIN
 SELECT
     b.booking_id,
     b.start_date,
@@ -35,7 +37,8 @@ SELECT
     pay.payment_id,
     pay.amount
 FROM Booking b
-INNER JOIN User u ON b.user_id = u.user_id
+INNER JOIN [User] u ON b.user_id = u.user_id
 INNER JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
-WHERE b.status = 'confirmed';
+WHERE b.status = 'confirmed'
+  AND b.total_price > 100;
