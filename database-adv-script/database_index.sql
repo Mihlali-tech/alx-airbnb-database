5a6c069891365aa7bcc3d22a587ac4bedfce2872
Mihlali Ncayo <mihlalincayo84@gmail.com>
2025-07-05 13:26:52 +0200

    Added [EXPLAIN ANALYZE]

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,8 @@
+-- Before creating index (example)
+-- EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 'some-user-id';
+
 -- User table
-CREATE INDEX idx_user_email ON User(email);
+CREATE INDEX idx_user_email ON [User](email);
 
 -- Booking table
 CREATE INDEX idx_booking_user_id ON Booking(user_id);
@@ -8,3 +11,6 @@ CREATE INDEX idx_booking_start_date ON Booking(start_date);
 
 -- Property table
 CREATE INDEX idx_property_location ON Property(location);
+
+-- After creating index (example)
+-- EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 'some-user-id';
